0800f6022a2feb4ba2be85d85f32a0285bcecdc2
sdwwwzaoy <zzh2019@mail.ustc.edu.cn>
2023-05-20 13:01:12 +0800

    init redis springboot

---
+++ redis-springboot/src/test/java/com/example/redisspringboot/RedisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
